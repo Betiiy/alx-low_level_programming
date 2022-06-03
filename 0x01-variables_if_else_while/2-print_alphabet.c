d5e18eec4868551eb477375ff07d064156a3e446
Betiiy <99496817+Betiiy@users.noreply.github.com>
2022-06-03 11:15:27 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints the alphabet in lowercase
+ * followed by a new line
+ * Return: 0
+ */
+int main (void)
+{
+char i;
+for (i='a' ; i <= 'z' ; i++)
+putchar(i);
+putchar('\n');
+}
