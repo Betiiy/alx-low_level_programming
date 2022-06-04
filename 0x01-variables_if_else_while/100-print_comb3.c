13cb5acccb1f23339377e9674e093bd48d6ef0cf
Betiiy <99496817+Betiiy@users.noreply.github.com>
2022-06-04 04:00:15 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,12 @@
 int main(void)
 {
 int x,y;
-for (int x=0 ; x<9 ; x++){
-for (int y=x+1 ;y < 10; y++){
-putchar((x%10)+ '0');
-putchar((y%10)+ '0');
+for (x = 0 ; x < 9 ; x++){
+for (y= x + 1 ;y < 10; y++){
+putchar((x % 10) + '0');
+putchar((y % 10) + '0');
 
-if (x==8 && y==9)
+if (x == 8 && y == 9)
 continue;
 
 putchar(',');
