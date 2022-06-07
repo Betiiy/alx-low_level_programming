7ca919c250f39f9801bd4adc5fa422bf1b923fbd
Betiiy <99496817+Betiiy@users.noreply.github.com>
2022-06-07 11:13:04 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,27 +2,27 @@
 #include "main.h"
 
 /**
- * _isalpha - entry point
+ * print_sign - entry point
  * description - a function that prints the sign of a number.
- * @c : character to check the case
+ * @n : integer tp print the sign 
  * Return: Always 0 (Success)
  */
 
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar('+');
-        return (1);
-    }
-    else if (n < 0)
-    {
-        _putchar('-');
-        return (-1);
-    }
-    else{
-        _putchar('0');
-        return (0);
-    }
-    return 0;
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else{
+_putchar('0');
+return (0);
+}
+return 0;
 }
