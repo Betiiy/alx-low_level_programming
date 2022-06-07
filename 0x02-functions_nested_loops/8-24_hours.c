9abc2f2976bef917eb60c7f2cde8f6ac02b4a091
Betiiy <99496817+Betiiy@users.noreply.github.com>
2022-06-07 12:21:27 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,23 +10,19 @@
 
 void jack_bauer(void)
 {
-  int i, j, k, l;
-  for (i=0 ; i< 3 ; i++)
-  {
-      for (j =0 ; j < 4 ; j++)
-      {
-         for ( k = 0 ; k < 6 ; k++)
-         {
-             for( l = 0 ; l < 10 ; l++)
-             {
-                 _putchar(i + '0');
-                 _putchar(j + '0');
-                 _putchar(':');
-                 _putchar(k + '0');
-                 _putchar(l + '0');
-                 _putchar('\n');
-             }
-         }
-      }
-  }
+	int hr;
+	int min;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
