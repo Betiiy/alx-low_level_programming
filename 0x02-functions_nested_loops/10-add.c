9b2b6f87afd804210ae04728274d4767d8c74f09
Betiiy <99496817+Betiiy@users.noreply.github.com>
2022-06-07 12:44:42 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - entry point
+ * description - a function that add two numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int x, int y)
+{
+ return (x + y);
+}
