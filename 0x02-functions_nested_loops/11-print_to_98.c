8f3eed1be3058572ec15c2fcb42906e2de44350d
Betiiy <99496817+Betiiy@users.noreply.github.com>
2022-06-07 12:58:55 -0700

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - entry point
+ * description - a function that print numbers.
+ * @n : number to check
+ * Return: Always 0 (Success)
+ */
+
+void print_to_98(int n)
+{
+    int i;
+
+    if (n < 98)
+    {
+        for (i = n ; i <= 98 ; i++ )
+        {
+            _putchar(i);
+            _putchar(',');
+            _putchar(' ');
+
+        }
+    }
+    else if (n > 98)
+    {
+        for (i = 98 ; i >= 98 ; i--)
+        {
+            _putchar(i);
+            _putchar(',');
+            _putchar(' ');
+        }
+
+    }
+    else
+    _putchar(i);
+}
