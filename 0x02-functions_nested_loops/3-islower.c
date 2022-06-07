db0f0ac1a4c36556550190cef02b4a704e072949
Betiiy <99496817+Betiiy@users.noreply.github.com>
2022-06-07 10:30:36 -0700

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,14 +4,14 @@
 /**
  * _islower - entry point
  * description -  a function that checks for lowercase character.
- * 
+ *
  * Return: Always 0 (Success)
  */
 
 int _islower(int c)
 {
-   if (c >= 97 && c <=122)
-   return (1);
-   else
-   return (0);
+if (c >= 97 && c <= 122)
+return (1);
+else
+return (0);
 }
