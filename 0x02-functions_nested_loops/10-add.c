e76b5367dc2fcf4cd88f6a3b2401d9d612bf5207
Betiiy <99496817+Betiiy@users.noreply.github.com>
2022-06-07 12:47:12 -0700

    Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,11 +4,12 @@
 /**
  * add - entry point
  * description - a function that add two numbers.
- *
+ * @x : first number
+ * @y : second number
  * Return: Always 0 (Success)
  */
 
 int add(int x, int y)
 {
- return (x + y);
+return (x + y);
 }
