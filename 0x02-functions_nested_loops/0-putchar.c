d9df5a66b38b0a383d5a02e790d5f18e6343afc6
Betiiy <99496817+Betiiy@users.noreply.github.com>
2022-06-07 08:50:26 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,12 +10,12 @@
 
 int main(void)
 {
-    char name[] = "_putchar";
-    int i;
-    for ( i = 0 ; i < 9 ;i++ )
-    {
-        _putchar(name[i]);
-    }
-    _putchar('\n');
-    return (0);
+char name[] = "_putchar";
+int i;
+for ( i = 0 ; i < 8 ;i++ )
+{
+ _putchar(name[i]);
+}
+_putchar('\n');
+return (0);
 }
