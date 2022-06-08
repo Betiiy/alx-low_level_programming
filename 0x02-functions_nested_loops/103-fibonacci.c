2362d44eb0b477ef8c0bdcc5650dd88bd132c0d6
Betiiy <99496817+Betiiy@users.noreply.github.com>
2022-06-08 02:22:08 -0700

    Task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,20 +8,20 @@
 
 int main(void)
 {
-	int i;
-	long int fib[50], sum = 2;
+int i;
+long int fib[50], sum = 2;
 
-	fib[0] = 1;
-	fib[1] = 2;
+fib[0] = 1;
+fib[1] = 2;
 	
-	for (i = 2; i < 50; i++)
-	{
-		fib[i] = fib[i - 1] + fib[i - 2];
-		if ((fib[i] % 2 == 0) && fib[i] < 4000000)
-        sum += fib[i];
+for (i = 2; i < 50; i++)
+{
+fib[i] = fib[i - 1] + fib[i - 2];
+if ((fib[i] % 2 == 0) && fib[i] < 4000000)
+sum += fib[i];
 		
-	}
-    printf("%ld\n", sum);
+}
+printf("%ld\n", sum);
 
-	return (0);
+return (0);
 }
