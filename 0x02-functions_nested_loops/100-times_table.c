45fd13e90c92361fb560f4c7ffe911ce81d3a225
Betiiy <99496817+Betiiy@users.noreply.github.com>
2022-06-08 01:40:21 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,48 +2,55 @@
 #include "main.h"
 
 /**
- * print_times_table - entry point
- * description - a function that table of  numbers.
- * @n : number to check
- * Return: Always 0 (Success)
+ * print_times_table - prints times table for numbers from 0-14
+ * @n: An input integer value
+ * Return: Nothing
  */
-
 void print_times_table(int n)
 {
-	int i;
-	int j;
+	int i, j;
 
-	if( n >= 0 && n <= 15)
+	if (n > 0 && n < 15)
 	{
-		for (i = 0 ; i < n ; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for(j = 0 ; j < n ; j++)
-			{
-				if((i * j) < 10)
-				{
-					_putchar((i * j) % 10 + '0');
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-				}
-				else if(((i * j) < 100 ) && ((i * j) >= 10 ))
-				{
-					_putchar((i * j) / 10 + '0');
-					_putchar((i * j) % 10 + '0');
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-				else if(((i * j) < 1000 ) && ((i * j) >= 100 ))
-				{
-					_putchar((i * j) / 100 + '0');
-					_putchar((i * j) % 100 + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
+			_putchar('0');
+			for (j = 1; j <= n; j++)
+				putformat(i * j);
 			_putchar('\n');
 		}
+	}
+}
+
+/**
+ * putformat - formatted characters to output
+ * @n: number to format
+ * Return: nothing
+ */
+void putformat(int n)
+{
+	if (n <= 9)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n + '0');
+	}
+	else if (n > 9 && n <= 99)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n / 100 + '0');
+		_putchar(n / 10 % 10 + '0');
+		_putchar(n % 10 + '0');
 	}
 }
